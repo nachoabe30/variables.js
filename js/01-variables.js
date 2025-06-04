7910b0b82a4bc92a9475fb266ad10cede4288abd
Nacho <abellesenignacio@gmail.com>
2025-06-03 21:34:26 -0300

    Muestra de las primeras variables creadas en Javascript

---
+++ js/01-variables.js
@@ -0,0 +1,21 @@
+//Comentario de una linea
+/*Comentario de muchas lineas*/
+
+//crear una variable (let- const- var) La dos primeras mas utilizadas
+//Existen reglas de declaracion de variables (no acentos ni ñ)
+//No es necesario poner el ; 
+
+let numero1;
+
+// = (Esta cajita que esta a mi derecha le guardo el valor del numero)
+let anio = 2025;
+
+//Mostrar el valor de las variables 
+//Metodo: es una accion que puedo hacer con la consola
+
+//NULL (absolutamente nada) 
+//Undefined (no definido o especificado)
+
+console.log('Hola Mundo')
+console.log(numero1, anio)
+
