ef07c3b011b0d2a1f83785b216b3b3abb5b2048e
Nacho <abellesenignacio@gmail.com>
2025-06-05 21:35:44 -0300

    Ingresamos diferentes metodos  de la funcion console (lenght, substring, charAt)

---
+++ js/ejercicios/02-tp1-ejercicio9.js
@@ -0,0 +1,15 @@
+//Escribe un programa que pida una frase y escriba las vocales que aparecen
+
+let frase = prompt('introduzca una frase al usuario: ');
+
+
+console.log(frase);
+
+//hola mundo
+//con lenght me va a decir cuantos caracteres tiene la frase que escribimos
+console.log(frase.length);
+//con substring me va a decir que caracteres va a mostrar (star number, final number)
+console.log(frase.substring(0,3));
+//com charat me va a decir solamente un solo caracter en la posicion que le idniquemos
+console.log(frase.charAt(5));
+
