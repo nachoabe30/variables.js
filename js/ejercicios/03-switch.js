7e3e9ed26a52aa2149e8ba283757b730c75a84b7
Nacho <abellesenignacio@gmail.com>
2025-06-10 20:41:47 -0300

    Agregamos ejercicio de switch case con diferentes condicionales y opciones para ingresar montos en cajero

---
+++ js/ejercicios/03-switch.js
@@ -0,0 +1,58 @@
+/*
+
+Cree un programa al estilo de un cajero automatico con las siguientes opciones:
+
+1- consultar el saldo
+
+2- ingresar dinero
+
+3- extraer dinero*/
+
+//switch case
+/* switch (opcion){
+    case "1":
+        Todas las lineas de codigo que quiero hacer si la opcion del usuario es 1
+        break;
+    case "2":
+    .
+    .
+    .
+    .
+    case n:
+    
+    default:
+        alert("opcion no valida");
+} */
+
+        const opcion = prompt(
+  "Seleccione una opcion: 1- consultar el saldo, 2-Ingresar dinero, 3-Extraer dinero"
+);
+
+let saldo = 10000;
+
+switch(opcion){
+    case "1":
+        alert("Su saldo es $ "+saldo);
+        break;
+    case "2":
+        const deposito = Number(prompt("Ingrese la cantidad de dinero que desea ingresar"))
+        if(deposito<0 && deposito>200000){
+            console.log(deposito);
+        saldo = saldo + deposito;
+        //Alt gr + comillas baticks ``, se puede hacer de esta forma el llamado de las variables
+        document.writeln(`Depositaste $ ${deposito}, tu saldo es: $${saldo}`) 
+
+        }
+        else{
+            alert("Ingresasaste un monto invalido")
+        }
+    break;
+
+    case "3":
+        break;
+    default:
+        alert("Ingresaste una opcion invalida");
+
+
+
+}
