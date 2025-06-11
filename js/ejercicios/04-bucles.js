c1f8382960b40b3e7a1aebd1e029e4ab09cef6c5
Nacho <abellesenignacio@gmail.com>
2025-06-10 21:32:21 -0300

    Agregamos rama de bucles y ejercicio con while

---
+++ js/ejercicios/04-bucles.js
@@ -0,0 +1,23 @@
+//Estrcuturas de repeticion
+
+//Tipos de bucles: while, for, do-while
+
+/*While - Mientras se cumpla tal condicion
+
+while(condicion logica){
+Todas las lineas de codigo que quiero repetir varias veces
+agregar alguna linea que haga que la condicion logica se deje de cumplir en algun momento
+}*/
+
+let renglon = 1;
+while(renglon <= 50){
+    document.writeln("<p>Renglon ${renglon}</p>");
+    renglon ++;
+
+}
+
+//do-while
+
+
+
+
