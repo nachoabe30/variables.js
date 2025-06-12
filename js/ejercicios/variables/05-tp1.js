1843ac650b27dafb7d171766865839f646547255
Nacho <abellesenignacio@gmail.com>
2025-06-12 19:58:15 -0300

    Ingresamos ejercicios del tp1 de variables hasta el el ejercicio 5

---
+++ js/ejercicios/variables/05-tp1.js
@@ -0,0 +1,6 @@
+/*Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.*/
+const num1 = prompt("Escriba el primer numero: ");
+const num2 = prompt("Escriba el segundo numero: ");
+const suma = parseInt(num1) + parseInt(num2);
+document.writeln("El numero total de los numeros es: "+suma);
+
