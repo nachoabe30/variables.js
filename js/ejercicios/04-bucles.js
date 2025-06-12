a0fa46389856cdce9a0de830a81cba34483fd871
Nacho <abellesenignacio@gmail.com>
2025-06-12 20:26:47 -0300

    Agregamos un ejemplo de las estructuras do while y for

--- js/ejercicios/04-bucles.js
+++ js/ejercicios/04-bucles.js
@@ -11,13 +11,38 @@ agregar alguna linea que haga que la condicion logica se deje de cumplir en algu
 
 let renglon = 1;
 while(renglon <= 50){
-    document.writeln("<p>Renglon ${renglon}</p>");
+    document.writeln(`<p>Renglon ${renglon}</p>`);
     renglon ++; // renglon = renglon +1
 
 }
 
 //do-while
+/* 
+do{
+todas las lineas de codigo que quiero
+alguna linea
+}while(condicion logica){
+}
+*/
 
+let fila = 1;
+do{
+document.writeln(`<p>fila ${fila}</p>`);
+fila ++;
+}while(fila <= 50);
+
+
+//for
+
+/*
+for(iniciar variable; condicion logica; incremento){
+        Todas las lineas de codigo que quiero que se repitan
+}
+*/
+
+for(let indice=10; indice >0; indice --){
+    document.writeln(`<p>Cuenta regresiva N ${indice}</p>`);
+}
 
 
 
