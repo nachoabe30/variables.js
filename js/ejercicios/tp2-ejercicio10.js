2ec51293b6675c761f7f602fb2385512fe09e5de
Nacho <abellesenignacio@gmail.com>
2025-06-17 20:33:32 -0300

    Completamos el punto 10 del tp 2 de bucles

--- js/ejercicios/tp2-ejercicio10.js
+++ js/ejercicios/tp2-ejercicio10.js
@@ -9,6 +9,11 @@ Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.*/
 //pedir al usuario las filas y columnas
 const filas = parseInt(prompt("Ingresa un numero de filas: "));
 const columnas = parseInt(prompt("Ingresa un numero de columnas:"));
+
+//Las variables mayormente se hacen arriba si son generales
+let totalCelda = filas * columnas;
+
+
 //muestra el resultado por consola
 console.log(filas, columnas)
 
@@ -21,7 +26,7 @@ document.writeln(`<table class='table table-striped'>
     for(let indiceFilas = 0; indiceFilas <filas; indiceFilas ++){ 
         document.writeln(`<tr>`);
         for(let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++){
-            document.writeln(`<td class= 'p-2'>35</td>`);
+            document.writeln(`<td class= 'p-2'>${totalCelda--}</td>`);
         }
         document.writeln(`</tr>`);
     }
