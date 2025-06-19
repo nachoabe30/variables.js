dd0992a60362c2f08368b3ad09b147e898cfe593
Nacho <abellesenignacio@gmail.com>
2025-06-19 20:19:34 -0300

    Ingresamos teoria de arrays

--- js/ejercicios/05-arrays.js
+++ js/ejercicios/05-arrays.js
@@ -12,14 +12,50 @@ const peliculas = []
 //array con elementos (valor y coma, valor y coma)
 const canciones = ["Batidora", 2010, true, "Musica ligera", "Mujer amante"] 
 
+//Se pueden cambiar (modificar), agregar y eliminar elementos de un array
 
 //mostrar un array
 console.log(peliculas);
 console.log(canciones);
 document.writeln(canciones);
 
-document.writeln("<ul>")
-for(let i=0; i<canciones.lenght; i++){
-    document.writeln(`<li>${cacniones[i]}</li>`)
+document.writeln(`<p>Mostrar un solo elemento del array: ${canciones[3]}</p>`)
+
+//mostrar un elemnto del array con un indice
+const indice = 0
+document.writeln(`<p>Mostrar un solo elemento del array: ${canciones[indice]}</p>`)
+
+//mostrar un array siempre con bucles
+document.writeln("<h2>Mostar el array de canciones</h2");
+
+document.writeln("<ul class='list-group'>");
+for (let i = 0; i < canciones.length; i++) {
+    document.writeln(`<li>${canciones[i]}</li>`);
 }
-document.writeln("</ul>")
+document.writeln("</ul>");
+
+
+//agregar elementos al array
+//metodos de agregar elementos al array
+
+document.writeln("<h2 class='mt-3>' Agregar un elemento al final del array</h2>")
+canciones.push('Like a rolling stone')
+
+document.writeln("<h2 class='mt-3>' Agregar un elemento al final del array cantidad de elementos: </h2>"+canciones.length)
+canciones.splice(4,0,'Feel good inc')
+
+//borrar elementos del array
+// borrar elementos del array
+//borrar primer elemento del array
+canciones.shift();
+canciones.shift();
+canciones.shift();
+canciones.shift();
+document.writeln(`<h2 class='mt-3'>Borrar un elemento del inicio del array - cantidad de elementos ${canciones.length}</h2>`);
+
+document.writeln("<ul class='list-group'>");
+for (let i = 0; i < canciones.length; i++) {
+    document.writeln(`<li class="list-group-item">${canciones[i]}</li>`);
+}
+
+document.writeln("</ul>");
