aab7347aa4bc8afedf18e58c0b265390492a33ee
Nacho <abellesenignacio@gmail.com>
2025-07-03 20:29:34 -0300

    Ingresamos teoria de herencia y polimorfismo de POO de la clase teorica 08 de clases y objetos

--- js/ejercicios/08-clases.js
+++ js/ejercicios/08-clases.js
@@ -29,23 +29,21 @@ class Persona {
     //Colocar el # tambien en el constructor
     #email;
     #direccion;
-
-
     
-
-    //constructor (encargado de crear las propiedades)
-    constructor(nombre,apellido,fechaNacimiento,dni,email,direccion,telefono,contrasenia){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.fechaNacimiento = fechaNacimiento;
-        this.dni = dni;
-        this.#email = email;
-        this.#direccion = direccion;
-        this.telefono = telefono;
-        this.contrasenia = contrasenia;
-        //se pueden agregar atributos sin que esten en el contructor
-        this.estado = true;
-    }
+    
+    constructor(nombre,apellido,fechaNacimiento,provinicia,dni,email,direccion,telefono,contrasenia){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.fechaNacimiento = fechaNacimiento;
+    this.provincia = provinicia;
+    this.dni = dni;
+    this.#email = email; // Aquí se asigna el email
+    this.#direccion = direccion; // Aquí se asigna la dirección
+    this.telefono = telefono;
+    this.contrasenia = contrasenia;
+    //Valor por defectos
+    this.estado = true;
+}
 
     //Propiedades computadas (getters y setters)
     //Get=leer un dato
@@ -58,6 +56,7 @@ class Persona {
     get getDireccion(){
         return this.#direccion
     }
+
     //Debemos poner el parametro nuevo entre los ()
     set setEmail(nuevoEmail){
         this.#email = nuevoEmail;
@@ -69,7 +68,7 @@ class Persona {
 
     //metodos (Si o si con parentesis (puede haber parametros))
     //Agregar en los metodos el privado
-    mostrarDatos(){
+    mostrarDatos(){ //Este metodo aplica polimorfismo
         //Todo lo que quiero que haga el metodo
         document.writeln(`<ul>
             <li>Nombre y Apellido: ${this.apellido}</li>
@@ -84,33 +83,83 @@ class Persona {
     }
 
     cambiarContrasenia(contraseniaActual, contraseniaNueva){
-        //this es el objeto que se esta ejecutando 
-        if(contraseniaActual === this.contraseniaActual){
-            this.contraseniaNueva = contraseniaNueva;
-            console.log("Cambiaste la contraseña");
-        }
+    if(contraseniaActual === this.contraseniaActual){ 
+        this.contraseniaNueva = contraseniaNueva;
+        console.log("Cambiaste la contraseña");
     }
+}
 
 }
 
+
+
 //Clase hija (extends de persona)
-class alumno extends Persona{
+class Alumno extends Persona{
     #notas
     #curso
-    constructor(nombre,apellido,fechaNacimiento,dni,email,direccion,telefono,contrasenia,comision,insignia,rollingCoins,asistencia,notas){
+    //Constructo nuevo de la clase aulumno
+    constructor(nombre,apellido,fechaNacimiento,provinicia,dni,email,direccion,telefono,contrasenia,comision,curso){
         //Invocamos al constructor de persona (super)
-        super(nombre,apellido,fechaNacimiento,dni,email,direccion,telefono,contrasenia,comision)
+        super(nombre,apellido,fechaNacimiento,provinicia,dni,email,direccion,telefono,contrasenia)
+        //Todas las propiedades nuevas de la clase hija
             this.comision = comision;
             this.insignia = [];
             this.rollingCoins = 0;
             this.asistencia = 0;
             this.#notas = [];
-            this.comision = comision;
             this.#curso = curso;
     }
-        
+
+    //Getters y setters 
+    get curso(){
+        return this.#curso;
+    }
+    set curso(curso){
+        this.#curso = curso;
+    }
+
+    //Agregar metodos (Si lo definimos al mismo metodo en la clase hija, se anula el mismo metodo del padre)
+    mostrarDatos(){
+        document.writeln(`<ol>
+            <li>Nombre y Apellido: ${this.apellido}</li>
+            <li>Email: ${this.getEmail}</li>
+            <li>Telefono: ${this.telefono}</li>
+            <li>Telefono: ${this.getDireccion}</li>
+            <li>Direccion: ${this.comision}</li>
+            <li>Notas: ${this.#notas}</li>
+            </ol>`)
+
+    }
+
+    caclularPromedio(){
+        let suma=0;
+        for(let i=0; i<this.#notas.length; i++){
+            suma += suma + this.#notas[i];
+        }
+        return suma / this.notas.length;
+    }
+
     }
 //Logica de mi programa (Usar esa clase que hemos creado (persona))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //New (Instanciamos una clase en especifico)
 const Maxi = new Persona("Mximiliano", "Gomez", "10/04/200", "Tucuman", "47353590","mx@gmail.com", "3815136085", "MaxiGomez30", "null");
@@ -124,9 +173,10 @@ const agus = new Persona(
     'Tucumán',
     '47665777',
     'agus@bulacio.com',
-    null,
     '38133456456',
-    '*1234AbC$'
+    '*1234AbC$',
+    '10',
+    '8',
 );
 
 //Quiero ver los datos en pantalla
@@ -134,15 +184,46 @@ Maxi.mostrarDatos();
 agus.mostrarDatos();
 
 //Mostrar el email antes de modificarlo (se pone normal el .email ya que hay getters y setters)
-document.writeln(`<h2>Consultar Email: ${Maxi.nuevoEmail}</h2>`);
 Maxi.setEmail = "Maxi@Gmail.com";
+document.writeln(`<h2>Consultar Email: ${Maxi.getEmail}</h2>`);
+
 
 //Encapsulamiento: No puedo manipular mis objetos facilmente
 //(Modificamos la propiedad del objeto)
 Maxi.mostrarDatos();
 
+//Objeto alumno
+const pau = new Alumno(
+  "Paula",
+  "Gramajo",
+  "10/05/2004",
+  "Tucuman",
+  "4766780",
+  "paula@gmail.com",
+  "38133456456",
+  "1234",
+  "Comision 13",
+  "Fulstack"
+);
 
-//herencia: es la capacidad de compartir atributos y metodos entre objetos
+console.log(pau);
+pau.mostrarDatos();
+
+pau.setDireccion = "San miguel de Tucuman";
+document.writeln(`<h2>Direccion: ${pau.getDireccion}</h2>`);
+
+pau.setNotas = 10;
+pau.setNotas = 8;
+pau.setNotas = 9;
+
+pau.mostrarDatos();
+pau.mostrarDatos();
+document.writeln(`<p>Promedio: ${pau.calcularPromedio()}</p>`);
+
+
+
+
+
 
 
 
